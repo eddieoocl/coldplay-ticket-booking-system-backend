b14e467e45bac85b7dafe5b4f868f0445781ef30
wonged6 <eddie.wong@oocl.com>
2024-12-06 15:57:08 +0800

    chore: init project

---
+++ src/main/java/com/coldplay/coldplay_ticket_booking_system/ColdplayTicketBookingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.coldplay.coldplay_ticket_booking_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ColdplayTicketBookingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ColdplayTicketBookingSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/coldplay/coldplay_ticket_booking_system/ColdplayTicketBookingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coldplay.coldplay_ticket_booking_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ColdplayTicketBookingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
